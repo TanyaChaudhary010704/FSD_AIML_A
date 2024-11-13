09befd6928de39c430cc4621c7d8ee0aba487844
TanyaChaudhary010704 <punch010704@gmail.com>
2024-11-13 09:59:34 +0530

    react practice files created

---
+++ JS/react-cdn.js
@@ -0,0 +1,10 @@
+const parent = document.getElementById('root');
+const element = React.createElement("h1", {}, "This is new Heading");
+const ele1 = React.createElement("h2", {}, "Shopping Cart");
+const item1 = React.createElement("li", {}, "Item 1");
+const item2 = React.createElement("li", {}, "Item 2");
+const list = React.createElement("ul", {},item1,item2);
+const root = ReactDOM.createRoot(parent);
+const para = <p>This is a para</p>;
+root.render(element);
+root.render([element, ele1, list, para]);

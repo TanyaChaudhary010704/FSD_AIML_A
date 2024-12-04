fb82ca526a719479fde25d80fd393f41b6c9c561
TanyaChaudhary010704 <punch010704@gmail.com>
2024-12-04 10:08:14 +0530

    added counter functionality

--- REACT/react-counter-app/src/App.jsx
+++ REACT/react-counter-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/counter" element={<Counter/>}></Route>
-        <Route path="/stopwatch" element={<h1>Stop Watch app</h1>}></Route>
+        <Route path="/stopwatch" element={<Stopwatch/>}></Route>
         <Route path="*" element={<h1>No Page available</h1>}></Route>
       </Routes>
 

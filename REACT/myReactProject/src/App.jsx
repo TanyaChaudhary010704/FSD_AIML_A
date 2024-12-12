f4ee9a50dcafa68890e94dc5886c7f02911f929e
TanyaChaudhary010704 <punch010704@gmail.com>
2024-12-12 15:00:04 +0530

    created registration page

--- REACT/myReactProject/src/App.jsx
+++ REACT/myReactProject/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import Registration from './components/Registration'
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}>
           <Route path="/login" element={<Login/>}></Route>
-          <Route path="/register" element={<h1>Registration</h1>}></Route>
+          <Route path="/register" element={<Registration/>}></Route>
         </Route>
         <Route path="/dashboard" element={<h1>Dashboard</h1>}></Route>
         <Route path="/logout" element={<h1>logout</h1>}></Route>

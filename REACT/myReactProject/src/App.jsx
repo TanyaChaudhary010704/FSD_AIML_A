0b144c4168cca9ebdb2180980b3b41376e02c894
TanyaChaudhary010704 <punch010704@gmail.com>
2024-12-12 15:41:37 +0530

    updated login and registration linkage

--- REACT/myReactProject/src/App.jsx
+++ REACT/myReactProject/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import Registration from './components/Registration'
 const App = () => {
+  const [regData, setRegData] = useState();
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/register" element={<Registration/>}></Route>
+          <Route path="/login" element={<Login regdata = {regData}/>}></Route>
+          <Route path="/register" element={<Registration setregdata = {setRegData}/>}></Route>
         </Route>
         <Route path="/dashboard" element={<h1>Dashboard</h1>}></Route>
         <Route path="/logout" element={<h1>logout</h1>}></Route>
--- REACT/myReactProject/src/components/Registration.jsx
+++ REACT/myReactProject/src/components/Registration.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-const Registration = () => {
+const Registration = ({setregdata}) => {
+  const [name, setname] = useState()
+  const [email, setemail] = useState()
+  const [pass, setpass] = useState()
+  function getData(e) {
+    e.preventDefault()
+    const data = { name, email, pass }
+    setregdata(data);
+    alert("Registration successful")
+  }
   return (
     <div>
       <h1>Register Here</h1>
       <form>
         <div class="mb-3">
           <label for="exampleInputName" class="form-label">Name</label>
-          <input type="text" class="form-control" id="exampleInputName" aria-describedby="emailHelp"/>
-          <div id="emailHelp" class="form-text">We'll never share your name with anyone else.</div>
+          <input type="text" onChange={(e)=>setname(e.target.value)} class="form-control" id="exampleInputName"/>
+          <div id="name" class="form-text">We'll never share your name with anyone else.</div>
         </div>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          <input type="email" onChange={(e)=>setemail(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
           <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1"/>
+          <input type="password" onChange={(e)=>setpass(e.target.value)} class="form-control" id="exampleInputPassword1"/>
         </div>
         <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" onClick={getData} class="btn btn-primary">Submit</button>
       </form>
     </div>
   )

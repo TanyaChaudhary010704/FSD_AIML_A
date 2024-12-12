a80584b69bace6bd9626ea85a93192a384ef34e1
TanyaChaudhary010704 <punch010704@gmail.com>
2024-12-12 14:54:21 +0530

    created login page

--- REACT/myReactProject/src/App.jsx
+++ REACT/myReactProject/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
-
+import Home from './components/Home'
+import Login from './components/Login'
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>}></Route>
-        <Route path="/login" element={<h1>Login</h1>}></Route>
-        <Route path="/registration" element={<h1>Registration</h1>}></Route>
+        <Route path="/" element={<Home/>}>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<h1>Registration</h1>}></Route>
+        </Route>
         <Route path="/dashboard" element={<h1>Dashboard</h1>}></Route>
         <Route path="/logout" element={<h1>logout</h1>}></Route>
         <Route path="*" element={<h1>No Page Available</h1>}></Route>
